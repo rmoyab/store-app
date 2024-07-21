75f0e4da27f201e90e16244df4c9312141db2fe7
darsay8 <darsay.one@gmail.com>
2024-07-21 16:39:39 -0400

    chore: Update environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  urlFB: process.env.URL_FB,
-  tokenFB: process.env.TOKEN_FB,
-  appName: process.env.APP_NAME,
+  urlFB: process.env['URL_FB'],
+  tokenFB: process.env['TOKEN_FB'],
+  appName: process.env['APP_NAME'],
 };
